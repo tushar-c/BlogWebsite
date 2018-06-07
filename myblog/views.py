47d98b3e8ee0ad1a11c2de6d1a809d15ef83c0dc
tushar-c <tushar.chat192@gmail.com>
2018-06-07 12:44:17 +0530

    Added Backends to Website

---
+++ myblog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'myblog/index.html', {'posts': posts})
+
+
+def about(request):
+    return render(request, 'myblog/about.html')
+
+
+def home(request):
+    return render(request, 'myblog/blog.html')
