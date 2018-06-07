293bf1e9406d0935b7737eb26f9e55706d9ec39c
tushar-c <tushar.chat192@gmail.com>
2018-06-07 17:03:44 +0530

    design changes

--- myblog/urls.py
+++ myblog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^blog', views.post_list, name='post_list'),
     url(r'^about', views.about, name='about'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_view, name='post_view'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!f9!%3=&3%=gvo$)zun6*dllueg-xi)gs*w2u-a!w9(9_121sh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
